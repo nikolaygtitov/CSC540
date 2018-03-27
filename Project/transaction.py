ab0611552a366011b31883d08b26e7e92a084e20
Nathan Schnoor <nathan@Nathans-MacBook-Pro.local>
2018-03-27 18:14:04 -0400

    Added transaction context manager

---
+++ Project/transaction.py
@@ -0,0 +1,11 @@
+
+from contextlib import contextmanager
+
+@contextmanager
+def sql_transaction(con):
+    try:
+        yield
+        con.commit()
+    except Exception as e:
+        con.rollback()
+        raise e
